f47f08b7ec917bb0571bd9a59bf8a997c7e14f56
Miguel Padilla <miguel.padilla.harmony2013@gmail.com>
2018-04-28 23:20:06 -0400

    Learning more about methods!

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -25,6 +29,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    methods_with_more_parameters();
 }
 
 fn first_word(s: &String) -> &str {
@@ -37,4 +43,13 @@ fn first_word(s: &String) -> &str {
     }
 
     &s[..]
+}
+
+fn methods_with_more_parameters() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
